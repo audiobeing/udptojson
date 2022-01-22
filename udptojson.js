0fa0fec3f8d141473742be81b8fb34617f404364
Ian Jarvis <audiobeing@Ians-MacBook-Pro.local>
2022-01-22 17:51:22 -0500

    adding udp-json

--- udptojson.js
+++ udptojson.js
@@ -1,14 +1,25 @@
 const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
+const JSONSocket = require('udp-json');
+
+
+// Listener socket
+const socket = dgram.createSocket('udp4');
+socket.bind(20777);
+const jsonSocket = new JSONSocket(socket)
+
 
 server.on('error', (err) => {
   console.log(`server error:\n${err.stack}`);
   server.close();
 });
+jsonSocket.on('message-complete', (msg, rinfo) => {
+    console.log('Message received', rinfo, msg);
+})
 
-server.on('message', (msg, rinfo) => {
-  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-});
+// server.on('message', (msg, rinfo) => {
+//   console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+// });
 
 server.on('listening', () => {
   const address = server.address();
@@ -23,13 +34,13 @@ server.bind(20777);
 // const dgram = require("dgram")
 // const JSONSocket = require('udp-json');
 
-// // Listener socket
-// const socket = dgram.createSocket('udp4');
-// socket.bind(20777, '127.0.0.1');
-// const jsonSocket = new JSONSocket(socket)
-// jsonSocket.on('message-complete', (msg, rinfo) => {
-//     console.log('Message received', rinfo, msg);
-// })
+// Listener socket
+const socket = dgram.createSocket('udp4');
+socket.bind(20777, '127.0.0.1');
+const jsonSocket = new JSONSocket(socket)
+jsonSocket.on('message-complete', (msg, rinfo) => {
+    console.log('Message received', rinfo, msg);
+})
 
 // Sender socket
 // const socket2 = dgram.createSocket('udp4');

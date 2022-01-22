13d371948e4e1605a19af96ee50ddf46951a375b
Ian Jarvis <audiobeing@Ians-MacBook-Pro.local>
2022-01-22 18:23:13 -0500

    trying different package

---
+++ f1.js
@@ -0,0 +1,32 @@
+import { F1TelemetryClient, constants } from "f1-telemetry-client";
+// or: const { F1TelemetryClient, constants } = require('f1-telemetry-client');
+const { PACKETS } = constants;
+
+/*
+*   'port' is optional, defaults to 20777
+*   'bigintEnabled' is optional, setting it to false makes the parser skip bigint values,
+*                   defaults to true
+*   'forwardAddresses' is optional, it's an array of Address objects to forward unparsed telemetry to. each address object is comprised of a port and an optional ip address
+*                   defaults to undefined
+*   'skipParsing' is optional, setting it to true will make the client not parse and emit content. You can consume telemetry data using forwardAddresses instead.
+*                   defaults to false
+*/
+const client = new F1TelemetryClient({ port: 20777 });
+client.on(PACKETS.event, console.log);
+client.on(PACKETS.motion, console.log);
+client.on(PACKETS.carSetups, console.log);
+client.on(PACKETS.lapData, console.log);
+client.on(PACKETS.session, console.log);
+client.on(PACKETS.participants, console.log);
+client.on(PACKETS.carTelemetry, console.log);
+client.on(PACKETS.carStatus, console.log);
+client.on(PACKETS.finalClassification, console.log);
+client.on(PACKETS.lobbyInfo, console.log);
+client.on(PACKETS.carDamage, console.log);
+client.on(PACKETS.sessionHistory, console.log);
+
+// to start listening:
+client.start();
+
+// and when you want to stop:
+client.stop();

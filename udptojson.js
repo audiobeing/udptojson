47e068e04a446e629adba1872d8c7a3e79860af4
Ian Jarvis <audiobeing@Ians-MacBook-Pro.local>
2022-01-22 19:59:34 -0500

    commented out some data

--- udptojson.js
+++ udptojson.js
@@ -6,12 +6,12 @@ const { PACKETS } = constants;
 
 const client = new F1TelemetryClient({ port: 20777 });
 client.on(PACKETS.event, console.log);
-client.on(PACKETS.motion, console.log);
+// client.on(PACKETS.motion, console.log);
 client.on(PACKETS.carSetups, console.log);
 client.on(PACKETS.lapData, console.log);
 client.on(PACKETS.session, console.log);
 client.on(PACKETS.participants, console.log);
-client.on(PACKETS.carTelemetry, console.log);
+// client.on(PACKETS.carTelemetry, console.log);
 client.on(PACKETS.carStatus, console.log);
 client.on(PACKETS.finalClassification, console.log);
 client.on(PACKETS.lobbyInfo, console.log);

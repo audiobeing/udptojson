dc16cf463d54cc0db955a53709f9e07b65dd02c6
Ian Jarvis <audiobeing@Ians-MacBook-Pro.local>
2022-01-22 21:30:30 -0500

    final classification and events

--- udptojson.js
+++ udptojson.js
@@ -8,13 +8,13 @@ const client = new F1TelemetryClient({ port: 20777 });
 client.on(PACKETS.event, console.log);
 // client.on(PACKETS.motion, console.log);
 // client.on(PACKETS.carSetups, console.log);
-client.on(PACKETS.lapData, console.log);
+// client.on(PACKETS.lapData, console.log);
 // client.on(PACKETS.session, console.log);
-client.on(PACKETS.participants, console.log);
+// client.on(PACKETS.participants, console.log);
 // client.on(PACKETS.carTelemetry, console.log);
 // client.on(PACKETS.carStatus, console.log);
 client.on(PACKETS.finalClassification, console.log);
-client.on(PACKETS.lobbyInfo, console.log);
+// client.on(PACKETS.lobbyInfo, console.log);
 // client.on(PACKETS.carDamage, console.log);
 // client.on(PACKETS.sessionHistory, console.log);
 

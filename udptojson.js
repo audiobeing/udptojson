8537a660f890e6ee12beeec32b2ab59b007a8ed9
Ian Jarvis <audiobeing@Ians-MacBook-Pro.local>
2022-01-22 20:09:19 -0500

    only participant and lobby

--- udptojson.js
+++ udptojson.js
@@ -5,18 +5,18 @@ const { F1TelemetryClient, constants } = require('@racehub-io/f1-telemetry-clien
 const { PACKETS } = constants;
 
 const client = new F1TelemetryClient({ port: 20777 });
-client.on(PACKETS.event, console.log);
+// client.on(PACKETS.event, console.log);
 // client.on(PACKETS.motion, console.log);
-client.on(PACKETS.carSetups, console.log);
+// client.on(PACKETS.carSetups, console.log);
 // client.on(PACKETS.lapData, console.log);
-client.on(PACKETS.session, console.log);
+// client.on(PACKETS.session, console.log);
 client.on(PACKETS.participants, console.log);
 // client.on(PACKETS.carTelemetry, console.log);
-client.on(PACKETS.carStatus, console.log);
-client.on(PACKETS.finalClassification, console.log);
+// client.on(PACKETS.carStatus, console.log);
+// client.on(PACKETS.finalClassification, console.log);
 client.on(PACKETS.lobbyInfo, console.log);
-client.on(PACKETS.carDamage, console.log);
-client.on(PACKETS.sessionHistory, console.log);
+// client.on(PACKETS.carDamage, console.log);
+// client.on(PACKETS.sessionHistory, console.log);
 
 // to start listening:
 client.start();
